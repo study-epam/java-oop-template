563e868aeac44ab01fcb56ea7013209ba6eca7f9
study-epam <g4595426@gmail.com>
2020-08-06 15:57:16 +0300

    some changes

---
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
@@ -0,0 +1,26 @@
+package com.epam.izh.rd.online.repository;
+
+import com.epam.izh.rd.online.entity.SchoolBook;
+
+public class SimpleSchoolBookRepository implements BookRepository<SchoolBook> {
+    @Override
+    public boolean save(SchoolBook book) {
+        return false;
+    }
+
+    @Override
+    public SchoolBook[] findByName(String name) {
+        return new SchoolBook[0];
+    }
+
+    @Override
+    public boolean removeByName(String name) {
+        return false;
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+    private SchoolBook[] schoolBooks;
+}
